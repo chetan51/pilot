046c93321e0679a9aa79d4ad7f3f24a73cbd1069
Jay Gokhale <simjega@gmail.com>
2013-09-20 16:29:52 -0700

    Cleaned up dictionarys in test_world.py

--- test/test_world.py
+++ test/test_world.py
@@ -5,29 +5,23 @@ import sys
 theta = int(sys.argv[1])
 time_steps = int(sys.argv[2])
 
-state = dict()
-state['x'] 			= 0.
-state['xdot'] 		= 0.
-state['xdotdot']	= 0.
-state['theta_int'] 	= 0.
-state['theta'] 		= np.deg2rad(theta)
-state['thetadot'] 	= 0.
-state['thetadotdot']= 0.
+state = {	'x' 			: 0.,
+	     	'xdot' 		: 0.,
+	     	'xdotdot' 		: 0.,
+	     	'theta_int' 	: 0.,
+	     	'theta' 		: np.deg2rad(theta),
+	     	'thetadot' 	: 0.,
+	     	'thetadotdot' 	: 0.}
 
 dt = 0.01
 
-params = dict()
-params['m'] 		= 1.
-params['k'] 		= 2.
-params['l'] 		= 1
+params = {	'm' 			: 1.,
+			'k'				: 2.,
+			'l'				: 1.}
 
+force = {'x' : 0}
 
 ip = PendulumWorld(state, dt, params)
-
-force = dict()
-force['x'] = 0;
-
-
 
 i = 0
 while i < time_steps or time_steps == -1:
