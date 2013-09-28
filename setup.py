f9379ecf833d54e87572beb5b63d7507be83c4ee
Chetan Surpur <chetan51@gmail.com>
2013-09-27 19:45:35 -0700

    Initial setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Pilot',
+      version='0.1',
+      description='AI program running on NuPIC',
+      author='Chetan Surpur',
+      author_email='chetan.surpur@gmail.com',
+      url='https://github.com/chetan51/pilot',
+      packages=['pilot']
+      )
