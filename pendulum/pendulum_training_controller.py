578703d2d906005514ef5dfc7ceb7b52ecfaea04
Chetan Surpur <chetan51@gmail.com>
2013-10-02 10:54:05 -0700

    Really rudimentary training algorithm

--- pendulum/pendulum_training_controller.py
+++ pendulum/pendulum_training_controller.py
@@ -1,10 +1,20 @@
 from core.controller import Controller
+from random import randrange
 
 
 class PendulumTrainingController(Controller):
 
+    def __init__(self, optimizer):
+        Controller.__init__(self, optimizer)
+        self.i = 0
+        self.force_x = 0
+
     def act(self, state, predictor):
-        return {'x': 0}
+        if self.i and self.i % 100 == 0:
+            self.force_x = randrange(-20, 20)
+
+        self.i += 1
+        return {'x': self.force_x}
 
     def cost(self, state):
         return 0
