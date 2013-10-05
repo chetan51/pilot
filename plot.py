a75b210acf357dfdff5b9566f606a73d3a1acdd8
Chetan Surpur <chetan51@gmail.com>
2013-10-05 15:44:58 -0700

    Removed unnecessary print statement

--- plot.py
+++ plot.py
@@ -2,7 +2,6 @@ from plotter.plot import csv_plot
 import sys
 
 if __name__ == "__main__":
-    print len(sys.argv)
     if len(sys.argv) >= 2:
         log_path = sys.argv[1]
         field_names = sys.argv[2:]
