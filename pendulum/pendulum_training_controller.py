fd40cbf50816909f42d18ffd93da89e466675a32
Chetan Surpur <chetan51@gmail.com>
2013-10-04 23:33:29 -0700

    Added noise to training controller

--- pendulum/pendulum_training_controller.py
+++ pendulum/pendulum_training_controller.py
@@ -1,12 +1,16 @@
 from core.controller import Controller
 from random import randrange
-from math import sin, exp
+from math import sin, exp, floor
 
 FORCE_RANGE = 40.
 B_MAX = 50000.
 B_MIN = 100.
 B_CENTER = 2000.
 B_SPREAD = 4000.
+R_MIN = 0.
+R_MAX = 10.
+R_CENTER = 20000.
+R_SPREAD = 1000.
 
 
 class PendulumTrainingController(Controller):
@@ -24,12 +28,22 @@ class PendulumTrainingController(Controller):
 
     def force(self, i):
         b = self.b(i)
-        return (FORCE_RANGE / 2) * sin(i / b)
+        f = (FORCE_RANGE / 2) * sin(i / b)
+        r = int(floor(self.r(i) * 1000))
+        n = randrange(-r, r) / 1000. if r else 0
+        f = f + n
+        return f
 
     def b(self, i):
-        r = B_MAX - B_MIN
+        range = B_MAX - B_MIN
         m = (-i + B_CENTER) / B_SPREAD
-        return -(r / (1 + exp(m))) + B_MAX
+        return -(range / (1 + exp(m))) + B_MAX
+
+    def r(self, i):
+        range = R_MAX - R_MIN
+        m = (-i + R_CENTER) / R_SPREAD
+        r = (range / (1 + exp(m)))
+        return r
 
     def heat(self, i):
         return 0
