74a0c2addaea9795bc71dd73f9ec203d9f33d3f7
Jay Gokhale <simjega@gmail.com>
2013-10-15 17:29:58 -0700

    Made prediction based off of expectation instead of highest probability

--- pendulum/pendulum_predictor.py
+++ pendulum/pendulum_predictor.py
@@ -15,5 +15,16 @@ class PendulumPredictor(Predictor):
         }
 
     def stateFromModelResult(self, result):
-        predictions = result.inferences['multiStepBestPredictions']
-        return {'theta': predictions[self.prediction_step]}
+        k_steps = self.prediction_step
+        expectation = 0.0
+        total_probability = 0.0
+        for i in result.inferences['multiStepPredictions'][k_steps]:
+            expectation += float(i)*float(result.inferences['multiStepPredictions'][k_steps][i])
+            total_probability += float(result.inferences['multiStepPredictions'][k_steps][i])
+        expectation = expectation / total_probability
+        return {'theta': expectation}
+
+    # previous version:
+    # def stateFromModelResult(self, result):
+    #     predictions = result.inferences['multiStepBestPredictions']
+    #     return {'theta': predictions[self.prediction_step]}
