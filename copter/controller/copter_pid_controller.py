db21be197a0db6fb8412bf00380173afc0083958
simjega <simjega@gmail.com>
2013-11-03 03:48:57 -0800

    Tweeked PID controller

--- copter/controller/copter_pid_controller.py
+++ copter/controller/copter_pid_controller.py
@@ -5,5 +5,5 @@ class CopterPIDController(CopterController):
 
     def chooseSpeed(self, state, predictor):
         y_error = self.target_y - state['y']
-        s = 1 * y_error
+        s = 0.3 * y_error
         return float(s)
