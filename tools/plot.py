d9740649d303d38481b1046ba18caed97809e206
simjega <simjega@gmail.com>
2013-11-03 15:38:40 -0800

    plotter possibly finally fixed

--- tools/plot.py
+++ tools/plot.py
@@ -12,8 +12,8 @@ def csv_plot(file_path, field_names):
     columns = []
     for i in field_names:
         columns.append(labels.index(i))
-    y = np.zeros([len(field_names), len(lines) - 2])
-    x = np.arange(len(lines) - 2)
+    y = np.zeros([len(field_names), len(lines)])
+    x = np.arange(len(lines))
     for col in range(len(lines[3:])):
         for row in range(len(field_names)):
             y[row][col] = float(lines[3 + col][columns[row]])
@@ -21,7 +21,7 @@ def csv_plot(file_path, field_names):
     plot_list = []
     for i in range(len(field_names)):
         plot_list.append(x)
-        plot_list.append(y[i][:])
+        plot_list.append(y[i][:-3])
         plot_list.append(colors[color_counter])
         color_counter += 1
 
